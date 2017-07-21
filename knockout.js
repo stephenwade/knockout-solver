243dcf11705be059dd3b1f080bee25b8cb8c9c0e
wadej <wadej@southern.edu>
2017-07-21 00:05:16 -0400

    Add useful functions to classes in knockout.js

--- knockout.js
+++ knockout.js
@@ -3,10 +3,35 @@ class BoardSpace {
     this.state = 'empty'; // possible states: empty, one, both
     this.player = undefined;
   }
+
+  move(player) {
+    if (this.state == 'empty') {
+      this.state = 'one';
+      this.player = player;
+      return true;
+    }
+    else if (this.state == 'one' && this.player == player) {
+      this.state = 'two';
+      return true;
+    }
+    return false;
+  }
+
+  getState() {
+    return this.state;
+  }
 }
 
 class Board {
   constructor(numSpaces) {
     this.spaces = Array(numSpaces).fill(BoardSpace());
+  }
+
+  move(player, spaceIndex) {
+    return this.spaces[spaceIndex].move(player);
+  }
+
+  getState(spaceIndex) {
+    return this.spaces[spaceIndex].getState();
   }
 }
