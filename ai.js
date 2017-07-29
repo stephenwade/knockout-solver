f6ef1bedf7a07bdf98542f368a25ad1cb6bbedee
Stephen Wade <stephen@stephenwade.me>
2017-07-29 19:11:38 -0400

    AI logs an error if it fails to make a move

--- ai.js
+++ ai.js
@@ -1,7 +1,16 @@
 class KnockoutAI {
   constructor(id, takeTurnFn) {
     window.addEventListener("knockout-nextturn", e => {
-      if (e.detail.player == id) takeTurnFn(id, this._takeTurn(e.detail));
+      if (e.detail.player != id) return;
+
+      const turn = this._takeTurn(e.detail);
+      const success = takeTurnFn(id, turn);
+      if (!success) {
+        takeTurnFn(id, []);
+        console.error(`${id} tried to make an invalid move, skipping turn
+attempted move: ${turn.toString()}
+next turn detail:`, e.detail);
+      }
     });
   }
 
