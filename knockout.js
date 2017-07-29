67ea1a36dcab9b30845db640550f348297d51c9b
Stephen Wade <stephen@stephenwade.me>
2017-07-28 20:11:13 -0400

    cleanup and reformat knockout.js

--- knockout.js
+++ knockout.js
@@ -1,16 +1,16 @@
-let canvas = document.getElementById('knockout-canvas');
+const canvas = document.getElementById("knockout-canvas");
 canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+canvas.height = window.innerHeight;
 
-window.onload = function() {
-  window.knockout = {};
+window.knockout = {};
 
-  knockout.game = new Game(18);
-  knockout.gui = new Gui(18);
-  knockout.players = [];
-  knockout.players.push(knockout.game.addPlayer("Stephen", "blue"));
-  knockout.players.push(knockout.game.addPlayer("Joshua", "red"));
+knockout.game = new Game(18);
+knockout.gui = new Gui(18);
+knockout.players = [];
+knockout.players.push(knockout.game.addPlayer("Stephen", "blue"));
+knockout.players.push(knockout.game.addPlayer("Joshua", "red"));
 
-  window.addEventListener('knockout-nextturn', e => console.log("next turn", e.detail));
-  knockout.game.startGame();
-}
+window.addEventListener("knockout-nextturn", e =>
+  console.log("next turn", e.detail)
+);
+knockout.game.startGame();
