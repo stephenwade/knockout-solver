2a80cf457e79e090add6a023297701220a49dc37
Stephen Wade <stephen@stephenwade.me>
2017-07-29 19:32:01 -0400

    format

--- knockout.js
+++ knockout.js
@@ -8,7 +8,11 @@ knockout.game = new Game(18);
 knockout.gui = new Gui(18);
 knockout.ais = [];
 let takeTurnFn = knockout.game.takeTurn.bind(knockout.game);
-knockout.ais.push(new KnockoutAI(knockout.game.addPlayer("AI 1", "blue"), takeTurnFn));
-knockout.ais.push(new KnockoutAI(knockout.game.addPlayer("AI 2", "red"), takeTurnFn));
+knockout.ais.push(
+  new KnockoutAI(knockout.game.addPlayer("AI 1", "blue"), takeTurnFn)
+);
+knockout.ais.push(
+  new KnockoutAI(knockout.game.addPlayer("AI 2", "red"), takeTurnFn)
+);
 
 knockout.game.startGame();
