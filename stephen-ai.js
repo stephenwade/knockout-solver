accb9007f7de722074054783d04ca5c0a91047b4
Stephen Wade <stephen@stephenwade.me>
2017-08-03 01:08:46 -0400

    an actual working AI

--- stephen-ai.js
+++ stephen-ai.js
@@ -1,5 +1,26 @@
 class StephenAI extends KnockoutAI {
-  _takeTurn(detail) {
-    return [1];
+  // https://oeis.org/A246688
+  _partitions(n, i = 1) {
+    if (n == 0) return [[]];
+    else if (i > n) return [];
+    else
+      return [
+        ...this._partitions(n - i, i + 1).map(x => [i, ...x]),
+        ...this._partitions(n, i + 1)
+      ];
+  }
+
+  _validMoves(board, total) {
+    return this._partitions(total).filter(p =>
+      p.every(move => board[move - 1].canMove())
+    );
+  }
+
+  _takeTurn({ board, player, dice, diceTotal }) {
+    const validMoves = this._validMoves(board, diceTotal);
+    if (validMoves.length)
+      return validMoves[0];
+    else
+      return [];
   }
 }
